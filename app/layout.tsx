63b667e9b1ade1f76f03da7d9be88d7b895d074f
KatyaPrusakova <katia.prusakova@gmail.com>
2025-01-07 16:26:57 +0700

    feat: clean up before implementing SDK

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,18 @@
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import './globals.css'
-// import { QProvider } from '@quorini' auth
+
 const poppins = Poppins({ subsets: ['latin'], weight: ['400', '500', '600', '700'], variable: '--font-poppins' })
+import { QClient } from "@quorini/qui-core"
+
+QClient.configure({
+		projectId: "YOUR_PROJECT_ID",
+		env: "YOUR_PROJECT_ENV",
+		gqlPaths: {
+	    queries: "ALL_EXPORTED_QUERIES",
+	    mutations: "ALL_EXPORTED_MUTATIONS",
+	  },
+})
 
 export const metadata: Metadata = {
   title: 'iDavos',
