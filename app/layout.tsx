8745df879f66e8d2a286bed5422a432c0db557b5
KatyaPrusakova <katia.prusakova@gmail.com>
2025-01-07 17:14:49 +0700

    fix: comment to run locally

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,16 +3,16 @@ import { Poppins } from 'next/font/google'
 import './globals.css'
 
 const poppins = Poppins({ subsets: ['latin'], weight: ['400', '500', '600', '700'], variable: '--font-poppins' })
-import { QClient } from "@quorini/qui-core"
+// import { QClient } from "@quorini/qui-core"
 
-QClient.configure({
-		projectId: "YOUR_PROJECT_ID",
-		env: "YOUR_PROJECT_ENV",
-		gqlPaths: {
-	    queries: "ALL_EXPORTED_QUERIES",
-	    mutations: "ALL_EXPORTED_MUTATIONS",
-	  },
-})
+// QClient.configure({
+// 		projectId: "YOUR_PROJECT_ID",
+// 		env: "YOUR_PROJECT_ENV",
+// 		gqlPaths: {
+// 	    queries: "ALL_EXPORTED_QUERIES",
+// 	    mutations: "ALL_EXPORTED_MUTATIONS",
+// 	  },
+// })
 
 export const metadata: Metadata = {
   title: 'iDavos',
